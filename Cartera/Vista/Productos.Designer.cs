4b175b394cbd55fed7c9ef22aa993ccf72a9873c
ROLAND RICARDO ARENAS ROMERO <roland.arenas.20@gmail.com>
2021-03-03 17:32:58 -0500

    cambios en producto

--- Cartera/Vista/Productos.Designer.cs
+++ Cartera/Vista/Productos.Designer.cs
@@ -30,11 +30,16 @@ namespace Cartera.Vista
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscarProducto = new System.Windows.Forms.TextBox();
+            this.BtBuscarProducto = new System.Windows.Forms.Button();
+            this.btTipoProducto = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 90);
             this.dataGridView1.Name = "dataGridView1";
@@ -42,11 +47,50 @@ namespace Cartera.Vista
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Producto";
+            // 
+            // txtBuscarProducto
+            // 
+            this.txtBuscarProducto.Location = new System.Drawing.Point(114, 41);
+            this.txtBuscarProducto.Name = "txtBuscarProducto";
+            this.txtBuscarProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtBuscarProducto.TabIndex = 2;
+            // 
+            // BtBuscarProducto
+            // 
+            this.BtBuscarProducto.Location = new System.Drawing.Point(247, 39);
+            this.BtBuscarProducto.Name = "BtBuscarProducto";
+            this.BtBuscarProducto.Size = new System.Drawing.Size(75, 23);
+            this.BtBuscarProducto.TabIndex = 3;
+            this.BtBuscarProducto.Text = "Buscar";
+            this.BtBuscarProducto.UseVisualStyleBackColor = true;
+            this.BtBuscarProducto.Click += new System.EventHandler(this.BtBuscarProducto_Click);
+            // 
+            // btTipoProducto
+            // 
+            this.btTipoProducto.Location = new System.Drawing.Point(359, 39);
+            this.btTipoProducto.Name = "btTipoProducto";
+            this.btTipoProducto.Size = new System.Drawing.Size(84, 23);
+            this.btTipoProducto.TabIndex = 4;
+            this.btTipoProducto.Text = "Tipo Producto";
+            this.btTipoProducto.UseVisualStyleBackColor = true;
+            // 
             // Productos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(931, 537);
+            this.Controls.Add(this.btTipoProducto);
+            this.Controls.Add(this.BtBuscarProducto);
+            this.Controls.Add(this.txtBuscarProducto);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Productos";
@@ -54,11 +98,16 @@ namespace Cartera.Vista
             this.Load += new System.EventHandler(this.Productos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscarProducto;
+        private System.Windows.Forms.Button BtBuscarProducto;
+        private System.Windows.Forms.Button btTipoProducto;
     }
 }
--- Cartera/Vista/Productos.cs
+++ Cartera/Vista/Productos.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Cartera.Controlador;
 
 namespace Cartera.Vista
 {
     public partial class Productos : Form
     {
+        CProducto producto = new CProducto();
         public Productos()
         {
             InitializeComponent();
@@ -20,14 +22,15 @@ namespace Cartera.Vista
         private void Productos_Load(object sender, EventArgs e)
         {
             CargarProducto();
+            autocompletar();
         }
         private void CargarProducto()
-        {
-            DataTable Dt_Productos = Conexion.consulta("SELECT Id_Producto, Nombre_Producto, Numero_contrato, Forma_pago, Valor_Total, Fecha_Venta, Valor_Entrada, Proyecto_Nombre, Nom_Tipo_Producto, Valor_Sin_interes, Cuotas_Sin_interes, Valor_Cuota_Sin_interes, Valor_Con_Interes, Cuotas_Con_Interes, Valor_Cuota_Con_Interes, Valor_Interes, Fecha_Recaudo, Observaciones, Fk_Id_Proyecto, Fk_Id_Tipo_Producto FROM Producto INNER join Proyecto on Id_Proyecto = Fk_Id_Proyecto INNER join Tipo_Producto on Id_Tipo_Producto = Fk_Id_Tipo_Producto");
-            dataGridView1.DataSource = Dt_Productos;
+        {           
+            dataGridView1.DataSource = producto.cargarProductos();
             dataGridView1.Columns["Id_Producto"].Visible = false;
             dataGridView1.Columns["Fk_Id_Proyecto"].Visible = false;
             dataGridView1.Columns["Fk_Id_Tipo_Producto"].Visible = false;
+            dataGridView1.Columns["Id_Financiacion"].Visible = false;
             dataGridView1.Columns[1].HeaderText = "Producto";
             dataGridView1.Columns[2].HeaderText = "Contrato";
             dataGridView1.Columns[3].HeaderText = "Forma Pago";
@@ -59,6 +62,52 @@ namespace Cartera.Vista
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void BtBuscarProducto_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = "";
+            dataGridView1.DataSource = producto.BuscarProductos(txtBuscarProducto.Text);
+            dataGridView1.Columns["Id_Producto"].Visible = false;
+            dataGridView1.Columns["Fk_Id_Proyecto"].Visible = false;
+            dataGridView1.Columns["Fk_Id_Tipo_Producto"].Visible = false;
+            dataGridView1.Columns["Id_Financiacion"].Visible = false;
+            dataGridView1.Columns[1].HeaderText = "Producto";
+            dataGridView1.Columns[2].HeaderText = "Contrato";
+            dataGridView1.Columns[3].HeaderText = "Forma Pago";
+            dataGridView1.Columns[4].HeaderText = "Valor Neto";
+            dataGridView1.Columns[5].HeaderText = "Fecha Venta";
+            dataGridView1.Columns[6].HeaderText = "Valor Entrada";
+            dataGridView1.Columns["Valor_Entrada"].Visible = false;
+            dataGridView1.Columns[7].HeaderText = "Proyecto";
+            dataGridView1.Columns[8].HeaderText = "Tipo Producto";
+            dataGridView1.Columns[9].HeaderText = "Valor sin Interes";
+            dataGridView1.Columns["Valor_Sin_interes"].Visible = false;
+            dataGridView1.Columns[10].HeaderText = "Cuotas sin Interes";
+            dataGridView1.Columns["Cuotas_Sin_interes"].Visible = false;
+            dataGridView1.Columns[11].HeaderText = "Valor Cuota Sin interes";
+            dataGridView1.Columns["Valor_Cuota_Sin_interes"].Visible = false;
+            dataGridView1.Columns[12].HeaderText = "Valor con Interes";
+            dataGridView1.Columns["Valor_Con_Interes"].Visible = false;
+            dataGridView1.Columns[13].HeaderText = "Cuotas con Interes";
+            dataGridView1.Columns["Cuotas_Con_Interes"].Visible = false;
+            dataGridView1.Columns[14].HeaderText = "Valor Cuota Con Interes";
+            dataGridView1.Columns["Valor_Cuota_Con_Interes"].Visible = false;
+            dataGridView1.Columns[15].HeaderText = "Porcentaje Interes";
+            dataGridView1.Columns["Valor_Interes"].Visible = false;
+            dataGridView1.Columns[16].HeaderText = "Fecha Recaudo";
+            dataGridView1.Columns["Fecha_Recaudo"].Visible = false;
+        }
+        void autocompletar()
+        {
+            AutoCompleteStringCollection lista = new AutoCompleteStringCollection();
+            //if para buscar por nombre o cedula
+            DataTable DtProductos = producto.cargarProductos();
+            for (int i = 0; i < DtProductos.Rows.Count; i++)
+            {
+                lista.Add(DtProductos.Rows[i]["Nombre_Producto"].ToString());
+            }
+            txtBuscarProducto.AutoCompleteCustomSource = lista;
         }
     }
 }
